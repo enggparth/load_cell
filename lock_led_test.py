db1e7a26afb5a11078acf995eebe578416910842
Parth Patel <engg.parthpatel@outlook.com>
2018-06-14 09:39:29 +0000

    four lock logic

--- lock_led_test.py
+++ lock_led_test.py
@@ -1,33 +1,81 @@
 # External module imports
 import RPi.GPIO as GPIO
 import time
-
+import sys
+import os
 # Pin Definitons:
-lock = 14
-led = 15
+DOOR_LOCK = [14,15,18,23]
+LED_STRIP = [24,25,8,7]
 
 
 # Pin setup
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(lock, GPIO.OUT)
-GPIO.setup(led, GPIO.OUT)
+GPIO.setup(DOOR_LOCK[0], GPIO.OUT)
+GPIO.setup(DOOR_LOCK[1], GPIO.OUT)
+GPIO.setup(DOOR_LOCK[2], GPIO.OUT)
+GPIO.setup(DOOR_LOCK[3], GPIO.OUT)
+GPIO.setup(LED_STRIP[0], GPIO.OUT)
+GPIO.setup(LED_STRIP[1], GPIO.OUT)
+GPIO.setup(LED_STRIP[2], GPIO.OUT)
+GPIO.setup(LED_STRIP[3], GPIO.OUT)
 #GPIO.setup(butPin, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Button pin set as input w/ pull-up
 
-# Initial state for LEDs:
-GPIO.output(lock, GPIO.LOW)
-GPIO.output(led, GPIO.LOW)
+# Initial state for door locks and led strips
+GPIO.setwarnings(False)
+GPIO.output(DOOR_LOCK[0], GPIO.LOW)
+GPIO.output(DOOR_LOCK[1], GPIO.LOW)
+GPIO.output(DOOR_LOCK[2], GPIO.LOW)
+GPIO.output(DOOR_LOCK[3], GPIO.LOW)
+GPIO.output(LED_STRIP[0], GPIO.LOW)
+GPIO.output(LED_STRIP[1], GPIO.LOW)
+GPIO.output(LED_STRIP[2], GPIO.LOW)
+GPIO.output(LED_STRIP[3], GPIO.LOW)
 
 
-print("Here we go! Press CTRL+C to exit")
+
+
+def door_lock_0():
+    GPIO.output(DOOR_LOCK[0],GPIO.HIGH)
+    time.sleep(0.5)
+    GPIO.output(DOOR_LOCK[0],GPIO.LOW)
+    time.sleep(1)
+    print("Door 1 opened")
+def door_lock_1():
+    GPIO.output(DOOR_LOCK[1],GPIO.HIGH)
+    time.sleep(0.5)
+    GPIO.output(DOOR_LOCK[1],GPIO.LOW)
+    time.sleep(1)
+    print("Door 2 opened")
+def door_lock_2():
+    GPIO.output(DOOR_LOCK[2],GPIO.HIGH)
+    time.sleep(0.5)
+    GPIO.output(DOOR_LOCK[2],GPIO.LOW)
+    time.sleep(1)
+    print("Door 3 opened")
+def door_lock_3():
+    GPIO.output(DOOR_LOCK[3],GPIO.HIGH)
+    time.sleep(0.5)
+    GPIO.output(DOOR_LOCK[3],GPIO.LOW)
+    time.sleep(1)
+    print("Door 4 opened")
+#Main prg to open door
 try:
-    while 1:
-        GPIO.output(led,GPIO.HIGH)
-        time.sleep(0.5)
-        GPIO.output(led,GPIO.LOW)
-        time.sleep(0.5)
-
-
-except KeyboardInterrupt:
-	GPIO.cleanup()
+    
+    if sys.argv[1]== "1":
+        door_lock_0()
         
-       
+    elif sys.argv[1]== "2":
+        door_lock_1()
+        
+    elif sys.argv[1]== "3":
+        door_lock_2()
+        
+    elif sys.argv[1]== "4":
+        door_lock_3()
+        
+    
+except KeyboardInterrupt:
+    print("Over")
+
+finally:
+    GPIO.cleanup()
